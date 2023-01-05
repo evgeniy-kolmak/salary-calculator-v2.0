804462b3af50693ae32174d47be0b61a43cccc94
evgeniy.kolmak@gmail.com <evgeniy-kolmak>
2023-01-05 09:41:14 +0000

    Updates

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ backButton === null || backButton === void 0 ? void 0 : backButton.addEventListe
 function appLoad(month) {
     var nomrHours = document.querySelector('.norm-hours');
     if (nomrHours) {
-        nomrHours.textContent = "\u041D\u043E\u0440\u043C\u0430 \u0447\u0430\u0441\u043E\u0432 ".concat(monthHours[month], " \u0432 \u044D\u0442\u043E\u043C \u043C\u0435\u0441\u044F\u0446\u0435");
+        nomrHours.innerHTML = "\u041D\u043E\u0440\u043C\u0430 \u0447\u0430\u0441\u043E\u0432 <b>".concat(monthHours[month], "</b> \u0432 \u044D\u0442\u043E\u043C \u043C\u0435\u0441\u044F\u0446\u0435");
     }
 }
 function getNightHours(tariff, nightHours) {
@@ -113,7 +113,7 @@ function getOverHours(workOutHour, month) {
 function createOfMarkup(arg) {
     var _a, _b;
     var titleWage = Array.from(document.querySelectorAll('.show-wage'));
-    titleWage[0].innerHTML = "\u0417\u0430\u0440\u043F\u043B\u0430\u0442\u0430 \u0437\u0430 \u043C\u0435\u0441\u044F\u0446 \u0441\u043E\u0441\u0442\u0430\u0432\u0438\u0442 <u>".concat(arg.wage.clear, "</u> byn").toUpperCase();
+    titleWage[0].innerHTML = "\u0417\u0430\u0440\u043F\u043B\u0430\u0442\u0430 \u0437\u0430 \u043C\u0435\u0441\u044F\u0446 \u0441\u043E\u0441\u0442\u0430\u0432\u0438\u0442 <br><u>".concat(arg.wage.clear, "</u> byn").toUpperCase();
     titleWage[1].innerHTML = "\u0412\u0441\u0435\u0433\u043E \u043D\u0430\u0447\u0438\u0441\u043B\u0435\u043D\u043E <u>".concat(arg.wage.dirty, "</u> byn").toUpperCase();
     var tittle = Array.from(document.querySelectorAll('.title'));
     var list = Object.entries(arg.list);
